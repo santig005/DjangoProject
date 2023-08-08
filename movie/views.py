3b9f1989fd48b12ae3e3a84144401353f9c2de3d
santig005 <sajhdg30@gmail.com>
2023-08-08 13:58:14 -0500

    Formulario de busqueda

--- movie/views.py
+++ movie/views.py
@@ -6,11 +6,7 @@ from .models import Movie
 def home(request):
     #return render(request, 'home.html',{'name':'Santiago Gomez'})
     searchTerm = request.GET.get('searchMovie')
-    if searchTerm:
-        movies=Movie.objects.filter(title__icontains=searchTerm)
-    else:
-        movies = Movie.objects.all()
-    return render(request, 'home.html',{'searchTerm':searchTerm,'movies':movies})
+    return render(request, 'home.html',{'searchTerm':searchTerm})
 
 def about(request):
     return HttpResponse('<h1>Welcome to About Page</h1>')
